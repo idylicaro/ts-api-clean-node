ec2e938e66549bed2956ec713bfdc6c3e6964886
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-06-13 19:24:48 -0300

    chore: jest coverage ignore imports

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
